7701fbb1ebb56f639639bfc67a5a3a02ec2c3854
CalamityLorenzo <CalamityLorenzo@users.noreply.github.com>
2024-02-17 14:46:59 +0000

    Add project files.

---
+++ ReversePolishCalc/TokenDefs/RPNToken.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ReversePolishCalc.TokenDefs
+{
+    public class RPNToken (string originalVal)
+    {
+    }
+
+    public class NumberRPNToken : RPNToken
+    {
+        public NumberRPNToken(string originalDigits):base(originalDigits)
+        {
+            processInput(originalDigits);
+        }
+
+        private void processInput(string originalVal)
+        {
+            this.NumberValue = double.Parse(originalVal);
+        }
+
+        public double NumberValue { get; private set; }
+    }
+
+    public class OperaetorRPNToken(string operatorVal);
+}
